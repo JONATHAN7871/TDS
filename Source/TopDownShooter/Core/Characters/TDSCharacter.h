ed0b82c93f63e37582a2f93cf10f9bf1304fb4dd
JONATHAN7871 <tkalenko.1995@mail.ru>
2025-06-03 04:02:47 +0300

    Update TDSCharacter.h

--- Source/TopDownShooter/Core/Characters/TDSCharacter.h
+++ Source/TopDownShooter/Core/Characters/TDSCharacter.h
@@ -1,9 +1,10 @@
-// Copyright 2025, CRAFTCODE, All Rights Reserved.
+// TDSCharacter.h - Обновленная версия
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "TDSCharacterMovementComponent.h"
 #include "TDSCharacter.generated.h"
 
 class UTDSCharacterMovementComponent;
@@ -13,53 +14,160 @@ class TOPDOWNSHOOTER_API ATDSCharacter : public ACharacter
 {
     GENERATED_BODY()
 
-#pragma region TDS Character Movement
 public:
-    /** Состояние бега, ходьбы, стрейфа, прицеливания */
-    UPROPERTY(BlueprintReadOnly, replicatedUsing=OnRep_IsWalkingState, Category=Character)
-    uint8 bIsWalkingState   : 1;
-    UPROPERTY(BlueprintReadOnly, replicatedUsing=OnRep_IsSprintingState, Category=Character)
-    uint8 bIsSprintingState : 1;
-    UPROPERTY(BlueprintReadOnly, replicatedUsing=OnRep_IsStrafingState, Category=Character)
-    uint8 bIsStrafingState : 1;
-    UPROPERTY(BlueprintReadOnly, replicatedUsing=OnRep_IsAimingState, Category=Character)
-    uint8 bIsAimingState    : 1;
+    ATDSCharacter(const FObjectInitializer& ObjectInitializer);
 
+#pragma region TDS Character Movement States
+public:
+    /** Основные состояния движения (реплицируются) */
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_IsWalkingState, Category="TDS Character")
+    uint8 bIsWalkingState : 1;
+    
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_IsSprintingState, Category="TDS Character")
+    uint8 bIsSprintingState : 1;
+    
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_IsStrafingState, Category="TDS Character")
+    uint8 bIsStrafingState : 1;
+    
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_IsAimingState, Category="TDS Character")
+    uint8 bIsAimingState : 1;
+
+    /** RepNotify функции для состояний */
     UFUNCTION()
     virtual void OnRep_IsWalkingState();
+    
     UFUNCTION()
     virtual void OnRep_IsSprintingState();
+    
     UFUNCTION()
     virtual void OnRep_IsStrafingState();
+    
     UFUNCTION()
     virtual void OnRep_IsAimingState();
 
-    UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+    /** Методы управления состояниями */
+    UFUNCTION(BlueprintCallable, Category="TDS Character", meta=(HidePin="bClientSimulation"))
     void SetWalking(bool NewWalk, bool bClientSimulation = false);
 
-    UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+    UFUNCTION(BlueprintCallable, Category="TDS Character", meta=(HidePin="bClientSimulation"))
     void SetSprinting(bool NewSprint, bool bClientSimulation = false);
 
-    UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+    UFUNCTION(BlueprintCallable, Category="TDS Character", meta=(HidePin="bClientSimulation"))
     void SetStrafing(bool NewStrafe, bool bClientSimulation = false);
 
-    UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+    UFUNCTION(BlueprintCallable, Category="TDS Character", meta=(HidePin="bClientSimulation"))
     void SetAiming(bool NewAim, bool bClientSimulation = false);
 #pragma endregion
 
-    
+#pragma region Custom Movement Controls
 public:
-    ATDSCharacter(const FObjectInitializer& ObjectInitializer);
-    
+    /** Методы для кастомных движений */
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StartWallRun();
+
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StopWallRun();
+
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StartSlide();
+
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StopSlide();
+
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StartProne();
+
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
+    void StopProne();
+
+    /** Проверки состояний кастомных движений */
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category="TDS Movement")
+    bool IsWallRunning() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category="TDS Movement")
+    bool IsSliding() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category="TDS Movement")
+    bool IsInProne() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category="TDS Movement")
+    ETDSCustomMovementMode GetCurrentCustomMovementMode() const;
+#pragma endregion
+
+#pragma region Engine Overrides
+public:
     virtual void Tick(float DeltaTime) override;
 
-    /** Access to custom movement component */
-    UFUNCTION(BlueprintCallable, Category = "Movement")
+    /** Доступ к кастомному компоненту движения */
+    UFUNCTION(BlueprintCallable, Category="TDS Movement")
     UTDSCharacterMovementComponent* GetTDSMovementComponent() const;
     
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 protected:
     virtual void BeginPlay() override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+#pragma endregion
+
+#pragma region Input Handling
+protected:
+    /** Input Actions для различных типов движения */
     
+    // Основные движения
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnWalkPressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnWalkReleased();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnSprintPressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnSprintReleased();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnStrafePressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnStrafeReleased();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnAimPressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnAimReleased();
+
+    // Кастомные движения
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnWallRunPressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnWallRunReleased();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnSlidePressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnSlideReleased();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnPronePressed();
+    
+    UFUNCTION(BlueprintCallable, Category="TDS Input")
+    void OnProneReleased();
+#pragma endregion
+
+#pragma region Blueprint Events
+protected:
+    /** События для Blueprint */
+    UFUNCTION(BlueprintImplementableEvent, Category="TDS Character")
+    void OnMovementStateChanged(bool bWalk, bool bSprint, bool bStrafe, bool bAim);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="TDS Character")
+    void OnCustomMovementStarted(ETDSCustomMovementMode MovementMode);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="TDS Character")
+    void OnCustomMovementEnded(ETDSCustomMovementMode MovementMode);
+#pragma endregion
 };
